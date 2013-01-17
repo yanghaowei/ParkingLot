5e61f8659c112acddd1d825373a38b5e895e6a18
haowei <hyang7@slb.com>
2013-01-17 13:49:12 +0800

    first

---
+++ Src/ParkingLot.rb
@@ -0,0 +1,8 @@
+class ParkingLot
+	def initialize(count)
+	@total_count = count
+	@avaliable_count = count
+	end
+	
+	attr_reader:avaliable_count 
+end
--- Test/ParkingLotTest.rb
+++ Test/ParkingLotTest.rb
@@ -1,7 +1,19 @@
-class ParkingLotTest < MiniTest::Unit::TestCase
+require '../Src/ParkingLot'
+require 'minitest/autorun'
+
+class ParkingLotTest < MiniTest::Unit::TestCase 
+
+	TOTAL_COUNT = 2
+	
     def setup
 	end
 	
 	def teardown
+	end
+	
+	def test_Dispaly_Avaliable_Total_Count_Given_Empty_ParkingLot
+
+	    @parkingLot = ParkingLot.new(TOTAL_COUNT)
+		assert_equal TOTAL_COUNT, @parkingLot.avaliable_count 
 	end
 end
